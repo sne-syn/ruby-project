b0f03140320ae2dac71f6c123824c7e7cc9e80f8
sne-syn <snezhana.syngur@gmail.com>
2021-02-27 16:17:33 +0000

    new: Add scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  root 'static_pages#landing_page'
-  get 'privacy_policy', to: 'static_pages#privacy_policy'
+  # resources :enrollments do
+  #   get :my_students, on: :collection
+  # end
+  
+  # devise_for :users
+  # resources :courses do
+  #   get :purchased, :pending_review, :created, on: :collection
+  #   resources :lessons
+  #   resources :enrollments, only: [:new, :create]
+  # end
+  
+  # resources :users, only: [:index, :edit, :show, :update]
+  get 'home/index'
+  # get 'activity', to: 'home#activity'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
