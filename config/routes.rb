9972bc347af6a21716d35abf1044fffdc49a9d33
sne-syn <snezhana.syngur@gmail.com>
2021-03-06 13:32:13 +0000

    new: Update with a scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,5 @@
 Rails.application.routes.draw do
-  # resources :enrollments do
-  #   get :my_students, on: :collection
-  # end
-  
-  # devise_for :users
-  # resources :courses do
-  #   get :purchased, :pending_review, :created, on: :collection
-  #   resources :lessons
-  #   resources :enrollments, only: [:new, :create]
-  # end
-  
-  # resources :users, only: [:index, :edit, :show, :update]
+  resources :courses
   get 'home/index'
-  # get 'activity', to: 'home#activity'
   root 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
